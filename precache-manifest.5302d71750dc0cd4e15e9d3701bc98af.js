a3ef4e0990a5e6dcb1e4c4540a611a4a0bddbc54
Dariusz Okonski <darek200180@gmail.com>
2019-05-23 22:49:11 +0100

    Updates

---
+++ precache-manifest.5302d71750dc0cd4e15e9d3701bc98af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22aa264ceed394d0009682bf1e6a8221",
+    "url": "/react-simple-counter-013/index.html"
+  },
+  {
+    "revision": "a96392bfe828b9f8382a",
+    "url": "/react-simple-counter-013/static/css/main.e0fdf191.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-simple-counter-013/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a96392bfe828b9f8382a",
+    "url": "/react-simple-counter-013/static/js/main.5252aa07.chunk.js"
+  },
+  {
+    "revision": "e7b544e91ec7a7cd2deb",
+    "url": "/react-simple-counter-013/static/js/runtime~main.c7b3499e.js"
+  }
+]);
